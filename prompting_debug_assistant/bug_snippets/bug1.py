05fedeecb9c6bbed4b2d6f123129f2bee311f5c7
zulfuqarovaseadet <zulfuqarovaaseadett@gmail.com>
2026-05-08 20:33:50 +0400

    Delete prompting_debug_assistant directory

--- prompting_debug_assistant/bug_snippets/bug1.py
+++
@@ -1,4 +0,0 @@
-def calculate_average(numbers):
-    return sum(numbers) / len(numbers)
-
-print(calculate_average([]))
--- prompting_debug_assistant/bug_snippets/bug2.js
+++
@@ -1,6 +0,0 @@
-function greetUser(name) {
-    console.log("Hello, " + name.toUpperCase() + "!");
-}
-
-// Test case with null (causes bug)
-greetUser(null);
