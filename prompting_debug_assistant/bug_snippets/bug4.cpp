922cb5a79ef6f43b1efaca91ce17786cfd3e42db
zulfuqarovaseadet <zulfuqarovaaseadett@gmail.com>
2026-05-11 14:06:23 +0400

    Update bug4.cpp

--- prompting_debug_assistant/bug_snippets/bug4.cpp
+++ prompting_debug_assistant/bug_snippets/bug4.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
+int calculateArea(int width, int height) {
+    int area = width * height;
+}
+
 int main() {
-    int* ptr = nullptr;
-    // Xəta: Null olan yaddaş ünvanını oxumağa çalışır (Segmentation fault).
-    std::cout << *ptr << std::endl; 
+    int w, h;
+    std::cout << "Eni və hündürlüyü daxil edin: ";
+    std::cin >> w >> h;
+
+    if (w = 0 || h = 0) {
+        std::cout << "Ölçülər sıfır ola bilməz!" << std::endl;
+    }
+
+    int result = calculateArea(w, h);
+    
+    if (result > 100); {
+        std::cout << "Çox böyük sahə!" << std::endl;
+    }
+
     return 0;
 }
