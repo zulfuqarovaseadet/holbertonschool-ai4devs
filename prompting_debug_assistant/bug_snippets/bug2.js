0b1f9e83243528be80fad85e17c8d0a8ba007a76
zulfuqarovaseadet <zulfuqarovaaseadett@gmail.com>
2026-05-11 14:04:38 +0400

    Update bug2.js

--- prompting_debug_assistant/bug_snippets/bug2.js
+++ prompting_debug_assistant/bug_snippets/bug2.js
@@ -1,4 +1,26 @@
-function greetUser(name) {
-    console.log("Hello, " + name.toUpperCase() + "!");
+import java.util.ArrayList;
+
+public class InventoryManager {
+    public static void main(String[] args) {
+        ArrayList<Integer> prices = new ArrayList<>();
+        prices.add(15);
+        prices.add(25);
+        prices.add(35);
+
+        int total = 0;
+        for (int i = 0; i <= prices.size(); i++) {
+            total += prices.get(i);
+        }
+
+        double average = total / prices.size();
+
+        if (average > 20) {
+            System.out.println("Orta qiymət yüksəkdir: " + average);
+        } else if (average = 20) {
+            System.out.println("Orta qiymət tamdır.");
+        }
+
+        String message = null;
+        System.out.println(message.toUpperCase());
+    }
 }
-greetUser(null);
